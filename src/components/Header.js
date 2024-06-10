2efa03aa179bb2e87b75c109008576278d5aee18
Krity <kritysingh20031996@gmail.com>
2024-06-10 13:58:53 +0530

    Created Header for Login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black">
+      <img className="w-40" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/1200px-Logonetflix.png" 
+      alt="Netflix-logo">
+      </img>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
