3ca44d933c798ad1f974e1aed331c097da0f2b04
Krity <kritysingh20031996@gmail.com>
2024-06-12 07:46:18 +0530

    Created login In form

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black">
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
       <img className="w-40" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/1200px-Logonetflix.png" 
       alt="Netflix-logo">
       </img>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
 
 const Login = () => {
   return (
     <div>
-      <Header/>
-      <img src='https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt='Bkg'/>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/e43aa8b1-ea06-46a5-abe3-df13243e718d/IN-en-20240603-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="Bkg"
+        />
+      </div>
+      <form className="absolute w-3/12 p-12 my-36 bg-black mx-auto right-0 left-0 text-white rounded-lg bg opacity-80">
+        <h2 className="font-bold text-3xl py-4">Sign In</h2>
+        <input
+          className="p-4 my-2 w-full bg-slate-700 rounded-md"
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          className="p-4 my-2 w-full bg-slate-700 rounded-md"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="p-4 my-4 bg-red-600 w-full rounded-lg">Sign In</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
